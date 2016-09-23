200182dc0ad52f629c0ea9b5cf0e1fdbf8ef3374
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-09-23 22:40:03 +0200

    Add action 'EQUAL'

--- calculator-test.js
+++ calculator-test.js
@@ -37,12 +37,20 @@ const testOperator = () => {
   action = {type: 'OPERATOR', value: '-'};
   stateAfter = {input: [1, '-'], current: '0'};
   expect(calculator(stateBefore, action)).toEqual(stateAfter);
+  stateBefore = {input: [1, '+', 1, '='], current: '2'};
+  action = {type: 'OPERATOR', value: '-'};
+  stateAfter = {input: [2, '-'], current: '0'};
+  expect(calculator(stateBefore, action)).toEqual(stateAfter);
 }
 
 let testDot = () => {
   let stateBefore = {input: [], current: '1'};
   let action = {type: 'DOT'};
   let stateAfter = {input: [], current: '1.'};
+  expect(calculator(stateBefore, action)).toEqual(stateAfter);
+  stateBefore = {input: [], current: '0'};
+  action = {type: 'DOT'};
+  stateAfter = {input: [], current: '0.'};
   expect(calculator(stateBefore, action)).toEqual(stateAfter);
   stateBefore = {input: [], current: '1.'};
   action = {type: 'DOT'};
@@ -54,10 +62,30 @@ let testDot = () => {
   expect(calculator(stateBefore, action)).toEqual(stateAfter);
 }
 
+let testEqual = () => {
+  let stateBefore = {input: [1, '+'], current: '2'};
+  let action = {type: 'EQUAL'};
+  let stateAfter = {input: [1, '+', 2, '='], current: '3'};
+  expect(calculator(stateBefore, action)).toEqual(stateAfter);
+  stateBefore = {input: [], current: '1'};
+  action = {type: 'EQUAL'};
+  stateAfter = {input: [1, '='], current: '1'};
+  expect(calculator(stateBefore, action)).toEqual(stateAfter);
+  stateBefore = {input: [2.3, '-'], current: '1'};
+  action = {type: 'EQUAL'};
+  stateAfter = {input: [2.3, '-', 1, '='], current: '1.3'};
+  expect(calculator(stateBefore, action)).toEqual(stateAfter);
+  stateBefore = {input: [], current: '0'};
+  action = {type: 'EQUAL'};
+  stateAfter = {input: [], current: '0'};
+  expect(calculator(stateBefore, action)).toEqual(stateAfter);
+}
+
 testDefault();
 testDigit();
 testOperator();
 testDot();
+testEqual();
 
 console.log('tests pass');
 
--- calculator.js
+++ calculator.js
@@ -8,16 +8,40 @@ let calculator = (state = {input: [], current: '0'}, action) => {
       } else {
         return Object.assign({}, state, {current: state.current + action.value});
       }
+
     case 'OPERATOR':
       if (typeof _.last(state.input) === 'string' && state.current === '0') {
-      return Object.assign({}, state, {input: [...state.input.slice(0, state.input.length-1),  action.value], current: '0'});
+        return Object.assign({}, state, {input: [...state.input.slice(0, state.input.length-1),  action.value], current: '0'});
       }
-      return Object.assign({}, state, {input: [...state.input, parseInt(state.current), action.value], current: '0'});
+      else if (_.last(state.input) === '=') {
+        return Object.assign({}, {input: [parseFloat(state.current), action.value], current: '0'});
+      }
+      return Object.assign({}, state, {input: [...state.input, parseFloat(state.current), action.value], current: '0'});
+
     case 'DOT':
       if (state.current.indexOf('.') !== -1) {
         return state;
       }
       return Object.assign({}, state, {current: state.current + '.'});
+
+      case 'EQUAL':
+      //to check dot at the end of current
+        if (state.input.length === 0 && state.current !== '0') {
+          return Object.assign({}, state, {input: [parseFloat(state.current), '=']});
+        }
+        if (state.input.length !== 0) {
+          let current = eval([...state.input, parseInt(state.current)].join(''));
+          if (current.toString().indexOf('.') !== -1) {
+            current = current.toFixed(8);
+            while (current.endsWith("0")) {
+              current = current.substring(0, current.length-1);
+            }
+          }
+          return Object.assign({}, state, {input: [...state.input, parseFloat(state.current), '='], current: current.toString()})
+        } else {
+          return state;
+        }
+
     default:
       return state;
   }
@@ -25,6 +49,6 @@ let calculator = (state = {input: [], current: '0'}, action) => {
 
 module.exports = {
     calculator
-}
+};
 
 console.log('calculator');
