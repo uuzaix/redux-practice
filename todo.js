beb83922a2237ec0c3f20df824d89cc63e9102eb
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-09-27 23:41:23 +0200

    Extract presentational components

--- todo.js
+++ todo.js
@@ -145,6 +145,39 @@ const FilterLink = ({
   );
 }
 
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li
+    onClick={onClick}
+    style= {{
+      textDecoration:
+        completed ?
+          'line-through' : 'none'
+    }}
+    >
+    {text}
+  </li>
+);
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
+
 const getVisibleTodos = (
   todos,
   filter
@@ -188,24 +221,14 @@ class TodoApp extends Component {
         }}>
         Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo => 
-            <li key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                });
-              }}
-              style= {{
-                textDecoration:
-                  todo.completed ?
-                    'line-through' : 'none'
-              }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id=> 
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          } />
         <p>
           Show:
           {' '}
