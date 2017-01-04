b5a633b7017baa513cf427ba31317b05b73694de
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-01-04 22:58:28 +0100

    Limit input length to fit in display

--- calculator-reducer.js
+++ calculator-reducer.js
@@ -3,33 +3,41 @@ const _ = require('lodash');
 let calculator = (state = { input: [], current: '0' }, action) => {
   switch (action.type) {
     case 'DIGIT':
-      if (state.current === '0') {
-        return Object.assign({}, state, { current: action.value.toString() });
-      } else if (_.last(state.input) === '=') {
-        return Object.assign({}, { input: [], current: action.value.toString() });
-      } else {
-        return Object.assign({}, state, { current: state.current + action.value });
+      if (state.current.length < 8 && (state.input.join('').length + state.current.length) < 20) {
+        if (state.current === '0') {
+          return Object.assign({}, state, { current: action.value.toString() });
+        } else if (_.last(state.input) === '=') {
+          return Object.assign({}, { input: [], current: action.value.toString() });
+        } else {
+          return Object.assign({}, state, { current: state.current + action.value });
+        }
       }
+      return state;
 
     case 'OPERATOR':
-      if (typeof _.last(state.input) === 'string' && state.current === '0') {
-        return Object.assign({}, state, { input: [...state.input.slice(0, state.input.length - 1), action.value], current: '0' });
-      }
-      else if (_.last(state.input) === '=') {
+      if (_.last(state.input) === '=') {
         return Object.assign({}, { input: [parseFloat(state.current), action.value], current: '0' });
       }
-      return Object.assign({}, state, { input: [...state.input, parseFloat(state.current), action.value], current: '0' });
+      if ((state.input.join('').length + state.current.length) < 17) {
+        if (typeof _.last(state.input) === 'string' && state.current === '0') {
+          return Object.assign({}, state, { input: [...state.input.slice(0, state.input.length - 1), action.value], current: '0' });
+        }
+        return Object.assign({}, state, { input: [...state.input, parseFloat(state.current), action.value], current: '0' });
+      }
+      return state;
 
     case 'DOT':
-      if (_.last(state.input) === '=') {
-        return Object.assign({}, { input: [], current: '0.' });
-      }
-      else {
-        if (state.current.indexOf('.') !== -1) {
-          return state;
+      if (state.current.length < 7 && (state.input.join('').length + state.current.length) < 19) {
+        if (_.last(state.input) === '=') {
+          return Object.assign({}, { input: [], current: '0.' });
         }
-        return Object.assign({}, state, { current: state.current + '.' });
-      }
+        else {
+          if (state.current.indexOf('.') !== -1) {
+            return state;
+          }
+          return Object.assign({}, state, { current: state.current + '.' });
+        }
+      } return state;
 
     case 'EQUAL':
       if (state.input.length === 0 && state.current !== '0' || _.last(state.input) === '=') {
